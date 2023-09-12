ef02de06acd4d527574f52999794f4a45becf274
gustavo-guarnieri-de-melo <gustavoguarnieridemelo@gmail.com>
2023-09-12 14:21:21 -0300

    Novo Projeto

---
+++ app/src/main/java/br/com/alura/ceep/model/Nota.kt
@@ -0,0 +1,14 @@
+package br.com.alura.ceep.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
+data class Nota(
+    @PrimaryKey
+    val id: String = UUID.randomUUID().toString(),
+    val titulo: String,
+    val descricao: String,
+    val imagem: String? = null
+)
